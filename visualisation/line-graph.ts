a5969bdbc20d635cf3280d2995c5e0c3b53bcc1e
Max Duval <max.duval@theguardian.com>
2021-10-29 19:29:39 +0100

    organise and comment code

--- visualisation/line-graph.ts
+++ visualisation/line-graph.ts
@@ -1,12 +1,95 @@
 import type { Progress } from "../progress.ts";
-import { axisLeft, line, scaleLinear, scaleTime } from "./d3.ts";
+import { line } from "../d3/shape.ts";
+import { scaleLinear, scaleTime } from "../d3/scale.ts";
 
-// https://stackoverflow.com/a/61829368
+// Remote data from JSON //
+/**
+ * Directory in which this file is found.
+ * https://stackoverflow.com/a/61829368
+ */
 const dir = new URL(".", import.meta.url).pathname;
 
 const data: Progress[] = JSON.parse(
   Deno.readTextFileSync(dir + "/../progress.json"),
 );
+
+// Dimensions //
+const width = 600;
+const height = 400;
+
+const yScale = scaleLinear()
+  .domain([0, 1])
+  .range([height, 0]); // note direction of y-axis in SVG
+
+const xScale = scaleTime()
+  .domain([new Date(2021, 1, 1), new Date()])
+  .range([0, width]);
+
+// SVG methods //
+/**
+ * Helper method to generate line paths
+ * @param path a string for the d="" attribute
+ * @param stroke colour of the stroke
+ */
+const path = (path: string | null, stroke = "black") =>
+  path ? `<path stroke="${stroke}" d="${path}" />` : "";
+
+/**
+ * The SVG as string, which we’ll save to a file.
+ */
+const svg = `<svg xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 ${width} ${height}"
+  width="${width}"
+  height="${height}">
+  <g class="lines" fill="none">
+    ${
+  path(
+    line<Progress>(
+      (d) => xScale(Date.parse(d.date)),
+      (d) => yScale(1 - d.percentage),
+    )(data),
+    "orange",
+  )
+}
+    ${
+  path(
+    line<Progress>(
+      (d) => xScale(Date.parse(d.date)),
+      (d) => yScale(1 - d.percentageSize),
+    )(data),
+    "darkorange",
+  )
+}
+
+
+
+    ${
+  path(
+    line<Progress>(
+      (d) => xScale(Date.parse(d.date)),
+      (d) => yScale(d.percentage),
+    )(data),
+    "darkblue",
+  )
+}
+    ${
+  path(
+    line<Progress>(
+      (d) => xScale(Date.parse(d.date)),
+      (d) => yScale(d.percentageSize),
+    )(data),
+    "blue",
+  )
+}
+  </g>
+</svg>`;
+
+Deno.writeTextFileSync(dir + "../public/progress.svg", svg);
+
+// deno run --allow-read --allow-write visualisation/line-graph.ts
+
+// Example from https://observablehq.com/@jeantimex/how-to-draw-a-basic-line-chart
+// WIP -- unused
 
 const _max = <T extends number | Date>(array: T[]): T =>
   array.reduce((curr, prev) => curr > prev ? curr : prev);
@@ -14,46 +97,6 @@ const _max = <T extends number | Date>(array: T[]): T =>
 const _min = <T extends number | Date>(array: T[]): T =>
   array.reduce((curr, prev) => curr < prev ? curr : prev);
 
-const RANGE = [0, 300];
-
-const yScale = scaleLinear()
-  .domain([
-    0,
-    1,
-    // min(data.map((d) => d.percentage)), // or 0
-    // max(data.map((d) => d.percentage)), // or 1
-  ])
-  .range(RANGE);
-
-const xScale = scaleTime()
-  .domain([
-    new Date(2021, 0, 1),
-    new Date(),
-  ])
-  .range([0, 600]);
-
-const linePath = line<Progress>()
+const _linePath = line<Progress>()
   .x((d) => xScale(Date.parse(d.date)))
   .y((d) => yScale(d.percentage));
-
-console.log(linePath);
-
-// deno run --no-check --allow-read visualisation/line-graph.ts
-
-/*
-
-
-
-
-  */
-// Example from https://observablehq.com/@jeantimex/how-to-draw-a-basic-line-chart
-
-var x = scaleLinear()
-  .domain([10, 130])
-  .range([0, 960]);
-
-xScale(20); // 80
-xScale(50); // 320
-
-// https://www.skypack.dev/view/d3-axis
-// https://www.skypack.dev/view/d3-scale
