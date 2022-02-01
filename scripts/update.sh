7087f15f2c5be3658ecc6ffb9b30fa98c56ace10
Max Duval <max.duval@theguardian.com>
2022-02-01 12:14:59 +0000

    exit on missing hash with update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+  echo 1>&2 "Missing commit hash!"
+  echo "Get one from See https://github.com/guardian/frontend/commits"
+  exit 2
+fi
+
 echo "Updating local data for $1";
 
 deno run -A commercial-bundle.ts $1
